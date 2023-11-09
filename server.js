16ddca47de21e949fe4c403eb72a4ce870bcd30e
Rajneesh <ssrajneesh693@gmail.com>
2023-11-09 14:28:28 +0530

    individual message

--- server.js
+++ server.js
@@ -116,10 +116,16 @@ app.get('/logout', async (req, res) => {
    } catch (error) {
      res.status(500).json({ error: error.message });
    }
- });
- 
+});
 
-
+app.get('/users', async (req, res) =>{
+   try {
+      const alluser = await Session.find()
+      res.status(200).json({"data" : alluser})
+   }catch{
+      res.status(401).json({ data: 'No users Found' });
+   }
+})
 
 // var app = require('express')();
 // var http = require('http').Server(app);
@@ -130,23 +136,77 @@ const io = require('socket.io')(4000,{
      origin:'*' //http://localhost:5001
    }
 });
-io.on('connection', client => { 
-   console.log("Connection Open!",client.id)
+const userSockets = {};
+io.on('connection', async client => {
+   const userEmail = client.handshake.query.userEmail;
+   userSockets[userEmail] = client.id
+   console.log(userSockets)
+   // try {
+   //    const updatedDocument = await Session.findOneAndUpdate(
+   //       { email: userEmail },
+   //       { id: client.id },
+   //       { new: false }
+   //    );
+
+   //    if (updatedDocument) {
+   //       console.log('Updated document:', updatedDocument);
+   //    } else {
+   //       console.log('No document found with the specified email.');
+   //    }
+   // } catch (err) {
+   //    console.error('Error updating document:', err);
+   // }
+   client.on("room", room => {
+      client.join(room)
+   })
+
+   client.on('event', async ({ message, userDeviceId, selectedEmail }) => {
+      const deviceidtoemit = await userSockets[selectedEmail]
+      console.log(userDeviceId,selectedEmail,deviceidtoemit,message)
+      client.to(deviceidtoemit).emit('emit', { message, userDeviceId, selectedEmail });
+    });
+
+   client.on('disconnect', data => {
+      console.log("Connection disconnected!", client.id);
+   });
+});
+
+
+// io.on('connection', client => {
+//    const userEmail = client.handshake.query.userEmail;
+//    console.log("Connection Open!",client.id,userEmail)
+//    Session.findOneAndUpdate(
+//       { email: userEmail },
+//       { id: client.id },
+//       { new: false },
+//       (err, updatedDocument) => {
+//          if (err) {
+//             console.error('Error updating document:', err);
+//          } else {
+//             if (updatedDocument) {
+//             console.log('Updated document:', updatedDocument);
+//             } else {
+//             console.log('No document found with the specified email.');
+//             }
+//          }
+//       }
+//     );
+   // console.log(`User with email "${userEmail}" and device ID "${client.id}" connected.`);
    // client.on('event', data =>{
    //    io.emit('emit', data, () => {
    //       console.log(arr1[0],data)
    //    })
    // })
-   client.on('event', data =>{
-      // io.to(arr1[0]).emit('emit', data, () => {
-         client.broadcast.emit('emit', data, () => {
-         // console.log(arr1[1],data)
-      })
-   })
-   client.on('disconnect',data => {
-      console.log("Connection disconnected!",client.id)
-   })
-});
+//    client.on('event', data =>{
+//       io.to(arr1[0]).emit('emit', data, () => {
+//          client.broadcast.emit('emit', data, () => {
+//          console.log(arr1[1],data)
+//       })
+//    })
+//    client.on('disconnect',data => {
+//       console.log("Connection disconnected!",client.id)
+//    })
+// });
 
 
 
